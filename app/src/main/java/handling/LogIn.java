8117d25f709c9e4603ff0eba3f443bddc15d54c2
Daniel <daniel_chrzanowski98@wp.pl>
2020-03-16 16:23:16 +0100

    User added, better organization

---
+++ app/src/main/java/handling/LogIn.java
@@ -0,0 +1,47 @@
+package handling;
+
+import android.os.AsyncTask;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LogIn extends AsyncTask<String, String, String> {
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected String doInBackground(String... queries) {
+        String resultString = null;
+
+        try {
+            DBConnection DBConnection = new DBConnection();
+            Connection con = DBConnection.getDBConnection();
+
+            if (con == null) {
+                System.out.println("Brak połączenia z internetem");
+
+            } else {
+                Statement stmt = con.createStatement();
+                ResultSet result = stmt.executeQuery(queries[0]);
+                result.next();
+                resultString = result.getString("nazwisko");
+
+            }
+        } catch (Exception ex) {
+            System.out.println("Exception in LogIn.doInBackground();");
+        }
+
+        return resultString;
+    }
+
+    @Override
+    protected void onPostExecute(String resultString) {
+        System.out.println("Zalogowano: " + resultString);
+    }
+
+
+}
---
+++ app/src/main/java/handling/User.java
@@ -0,0 +1,19 @@
+package handling;
+
+public class User {
+
+    public static User user;
+    public String login, password, name, surname, email;
+
+    private User() {
+    }
+
+    public static User getUser() {
+        if (user == null) {
+            user = new User();
+        }
+
+        return user;
+    }
+
+}
