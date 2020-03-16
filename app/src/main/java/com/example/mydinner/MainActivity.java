03e3166cb3a847e5e2d6758d3c5675d6950ff6b7
Daniel <daniel_chrzanowski98@wp.pl>
2020-03-16 15:36:24 +0100

    DBconn

--- app/src/main/java/com/example/mydinner/MainActivity.java
+++ app/src/main/java/com/example/mydinner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mydinner;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -9,6 +10,12 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import handling.DBConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +32,47 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        System.out.println("AAAAAAAAAAAAAAAAAAAAA");
+
+        Doregister doregister = new Doregister();
+        doregister.execute();
     }
 
+
+
+    public class Doregister extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBB");
+            try {
+                DBConnection DBConnection = new DBConnection();
+                Connection con = DBConnection.getDBConnection();
+
+                if (con == null) {
+                    System.out.println("BLAD 2");
+                } else {
+
+                    String query = "select * from pracownicy";
+
+                    Statement stmt = con.createStatement();
+                    ResultSet result = stmt.executeQuery(query);
+                    System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBB");
+                    result.next();
+                    String nazwisko = result.getString("nazwisko");
+
+                    System.out.println("nazwisko: " + nazwisko);
+
+                }
+            } catch (Exception ex) {
+                System.out.println("BLAD 1");
+            }
+
+            return "wynik";
+        }
+
+
+    }
 }
+
